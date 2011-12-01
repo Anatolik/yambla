bff283208d0cb2ab05f3097e75347aa256d71b79
Anatoliy Kaverin <akaverin@lohika.com>
2011-12-01 11:47:30 +0200

    fixed layout constant

--- src/main/java/com/lohika/yambla/StatusActivity.java
+++ src/main/java/com/lohika/yambla/StatusActivity.java
@@ -1,11 +1,14 @@
 package com.lohika.yambla;
 
 import android.app.Activity;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 import winterwell.jtwitter.Twitter;
+import winterwell.jtwitter.TwitterException;
 
 public class StatusActivity extends Activity implements View.OnClickListener {
 
@@ -15,17 +18,18 @@ public class StatusActivity extends Activity implements View.OnClickListener {
 
     /**
      * Called when the activity is first created.
-     * @param savedInstanceState If the activity is being re-initialized after 
-     * previously being shut down then this Bundle contains the data it most 
-     * recently supplied in onSaveInstanceState(Bundle). <b>Note: Otherwise it is null.</b>
+     *
+     * @param savedInstanceState If the activity is being re-initialized after
+     *                           previously being shut down then this Bundle contains the data it most
+     *                           recently supplied in onSaveInstanceState(Bundle). <b>Note: Otherwise it is null.</b>
      */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.status);
 
-        editText = (EditText)findViewById(R.id.status_edit);
-        updateButton = (Button)findViewById(R.id.status_button);
+        editText = (EditText) findViewById(R.id.status_edit);
+        updateButton = (Button) findViewById(R.id.status_button);
 
         updateButton.setOnClickListener(this);
 
@@ -35,7 +39,29 @@ public class StatusActivity extends Activity implements View.OnClickListener {
 
     @Override
     public void onClick(View view) {
-        twitter.setStatus(editText.getText().toString());
+        String status = editText.getText().toString();
+        new PostToTwitter().execute(status);
+    }
+
+    class PostToTwitter extends AsyncTask<String, Integer, String> {
+
+        @Override
+        protected String doInBackground(String... statuses) {
+            try {
+                winterwell.jtwitter.Status status = twitter.updateStatus(statuses[0]);
+                return status.text;
+
+            } catch (TwitterException e) {
+                e.printStackTrace();
+                return "Failed to post!";
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            Toast.makeText(StatusActivity.this, result, Toast.LENGTH_LONG).show();
+
+        }
     }
 }
 
