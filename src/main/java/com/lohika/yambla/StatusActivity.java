34e1054ea7d87e494aa905cc57b0737114e257e9
Anatoliy Kaverin <akaverin@lohika.com>
2011-12-30 06:02:17 +0200

    added counter

--- src/main/java/com/lohika/yambla/StatusActivity.java
+++ src/main/java/com/lohika/yambla/StatusActivity.java
@@ -1,19 +1,25 @@
 package com.lohika.yambla;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 import winterwell.jtwitter.Twitter;
 import winterwell.jtwitter.TwitterException;
 
-public class StatusActivity extends Activity implements View.OnClickListener {
+public class StatusActivity extends Activity implements View.OnClickListener, TextWatcher {
 
+    public static final int TWIT_LENGTH = 140;
     private Twitter twitter;
     private EditText editText;
+    private TextView textCount;
 
     /**
      * Called when the activity is first created.
@@ -28,9 +34,14 @@ public class StatusActivity extends Activity implements View.OnClickListener {
         setContentView(R.layout.status);
 
         editText = (EditText) findViewById(R.id.status_edit);
+        editText.addTextChangedListener(this);
 
         Button updateButton = (Button) findViewById(R.id.status_button);
         updateButton.setOnClickListener(this);
+        
+        textCount = (TextView) findViewById(R.id.status_textCount);
+        textCount.setText(Integer.toString(TWIT_LENGTH));
+        textCount.setTextColor(Color.GREEN);
 
         //noinspection deprecation
         twitter = new Twitter("askme", "123456");
@@ -41,6 +52,26 @@ public class StatusActivity extends Activity implements View.OnClickListener {
     public void onClick(View view) {
         String status = editText.getText().toString();
         new PostToTwitter().execute(status);
+    }
+
+    @Override
+    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+    }
+
+    @Override
+    public void onTextChanged(CharSequence s, int start, int before, int count) {
+    }
+
+    @Override
+    public void afterTextChanged(Editable statusText) {
+        int count = TWIT_LENGTH - statusText.length();
+        textCount.setText(Integer.toString(count));
+        textCount.setTextColor(Color.GREEN);
+        if (count < 20 && count >= 0) {
+            textCount.setTextColor(Color.YELLOW);
+        } else if (count < 0){
+            textCount.setTextColor(Color.RED);
+        }
     }
 
     private class PostToTwitter extends AsyncTask<String, Integer, String> {
